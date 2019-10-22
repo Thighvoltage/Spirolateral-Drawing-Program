d0b80e543a663e1a53cc23e0cafe463cfb5ae85b
Thighvoltage <48503740+Thighvoltage@users.noreply.github.com>
2019-10-22 22:18:16 +1300

    GUI Main Menu
    Added
    - Menu buttons and space to display the list of spirolaterals

--- Spirolateral Drawing Program.py
+++ Spirolateral Drawing Program.py
@@ -14,7 +14,64 @@ class Spirolateral():
 
 class GUI:
     def __init__(self, master):
-        print(0)
+        """Defines all GUI frames, widgets, turtle, and constants.
+        """
+
+        COLOUR_BG = "pale green"
+        PAD_LX = 20
+        PAD_LY = 10
+        PAD_BX = 3
+        PAD_BY = 3
+        WIDTH = 10
+
+        self.master = master
+        self.frame_menu = Frame(master, bg = COLOUR_BG)
+        self.frame_menu.grid(row = 0, column = 0, columnspan = 2,
+                             sticky = 'nesw')
+
+        self.label_menu = Label(self.frame_menu, text = "Menu", bg = COLOUR_BG)
+
+        self.button_add = Button(self.frame_menu, text = "Add a spirolateral",
+                                 command = self.spiro_add)
+        self.button_remove = Button(self.frame_menu, text = "Remove a spiro"
+                                    + "lateral", command = self.spiro_remove)
+        self.button_draw = Button(self.frame_menu, text = "Draw a spirolateral"
+                                  , command = self.spiro_draw)
+        self.button_save = Button(self.frame_menu, text = "Save spirolateral "
+                                  + "list", command = self.save)
+        self.button_load = Button(self.frame_menu, text = "Load spirolateral "
+                                  + "list", command = self.load)
+        self.button_quit = Button(self.frame_menu, text = "Quit",
+                                  command = self.quit)
+
+        self.label_menu.grid(row = 0, column = 0, rowspan = 2, padx = PAD_LX)
+
+        self.button_add.grid(row = 0, column = 1, padx = PAD_BX, pady = PAD_BY,
+                             sticky = EW)
+        self.button_remove.grid(row = 1, column = 1, padx = PAD_BX,
+                                pady = PAD_BY, sticky = EW)
+        self.button_draw.grid(row = 1, column = 2, padx = PAD_BX,
+                              pady = PAD_BY, sticky = EW)
+        self.button_save.grid(row = 0, column = 2, padx = PAD_BX,
+                              pady = PAD_BY, sticky = EW)
+        self.button_load.grid(row = 0, column = 3, padx = PAD_BX,
+                              pady = PAD_BY, sticky = EW)
+        self.button_quit.grid(row = 1, column = 3, padx = PAD_BX,
+                              pady = PAD_BY, sticky = EW)
+
+        self.frame_secondary = Frame(master)
+        self.frame_secondary.grid(row = 1, column = 1, sticky = NW)
+
+        self.frame_spiro = Frame(master)
+        self.frame_spiro.grid(row = 1, column = 0, rowspan = 2, sticky = NW)
+
+        self.label_spiro = Label(self.frame_spiro, text = "Spirolaterals:")
+        self.label_spiro.grid(row = 0, column = 0, padx = PAD_LX,
+                              pady = PAD_LY, sticky = NW)
+
+        self.label_spiros = Label(self.frame_spiro, justify = LEFT)
+        self.label_spiros.grid(row = 1, column = 0, padx = PAD_LX,
+                               sticky = NW)
 
     def spiro_add(self):
         print(1)
