6ff6a4cedaaac88dc647373e1de36df8078f9023
Thighvoltage <48503740+Thighvoltage@users.noreply.github.com>
2019-10-23 18:30:11 +1300

    Spirolateral Addition and Number Validation
    What's new
    - Spirolaterals can now be added to the program(although they aren't yet listed)
    - Developed the number validation function
    - Added a constant for the maximum number of spirolaterals the program can hold

--- Spirolateral Drawing Program.py
+++ Spirolateral Drawing Program.py
@@ -102,7 +102,75 @@ class GUI:
         self.turtle = turtle.RawTurtle(self.canvas)
 
     def spiro_add(self):
-        print(1)
+        # Stops function from running when there's the max amount of spiros
+        # by putting the rest of the function indented in the if statement
+        if self.option_stop(MAX_SPIRO, "You can only have {} spirolaterals."
+                            .format(MAX_SPIRO)) != -1:
+
+            # Changes labels and buttons, and adds labels, entries, and buttons
+            # to the grid
+            self.label_prompt1.configure(text = "Name:")
+            self.label_prompt2.configure(text = "Segments:")
+            self.label_prompt3.configure(text = "Angle:")
+
+            self.label_prompt1.grid(row = 0, column = 0, sticky = W)
+            self.label_prompt2.grid(row = 1, column = 0, sticky = W)
+            self.label_prompt3.grid(row = 2, column = 0, sticky = W)
+
+            self.label_response1.grid(row = 0, column = 2, sticky = W)
+            self.label_response2.grid(row = 1, column = 2, sticky = W)
+            self.label_response3.grid(row = 2, column = 2, sticky = W)
+
+            self.entry1.grid(row = 0, column = 1)
+            self.entry2.grid(row = 1, column = 1)
+            self.entry3.grid(row = 2, column = 1)
+
+            self.button_enter.configure(command = self.check_add)
+
+            self.button_enter.grid(row = 3, column = 0)
+
+    def check_add(self):
+        """Checks that entries are valid then adds a spirolateral if they are
+        """
+        name = self.entry1.get()
+        if name == "":
+            self.label_response1.configure(text = "Enter a name.")
+
+        # Checks for duplicate name
+        for index in range(len(spiros)):
+            if name == spiros[index].name:
+                self.label_response1.configure(text = "There's already a "
+                                               + "spirolateral called {}."
+                                               .format(name))
+                name = ""
+
+        segment = check_num(self.entry2, self.label_response2,
+                            "Can't have 0 or less segments.",
+                            MIN_CHOICE, float('inf'), int)
+        angle = check_num(self.entry3, self.label_response3,
+                          "Angle can't be less than 0° or greater than 360°.",
+                          MIN_CHOICE, 360, int)
+
+        # Adds a spirolateral if all the entry requirements have been met
+        if name != "" and segment != -1 and angle != -1:
+            spiros.append(Spirolateral(name, segment, angle))
+
+            self.spiro_print()
+            self.clear()
+
+            self.label_prompt1.configure(text = "Spirolateral added.")
+            self.label_prompt1.grid(row = 0, column = 0)
+
+        # Rest of function checks if entries with responses now have their
+        # requirements met and clears the response if they have
+        elif name != "" and self.label_response1.cget("text") != "":
+            self.label_response1.configure(text = "")
+
+        elif segment != -1 and self.label_response2.cget("text") != "":
+            self.label_response2.configure(text = "")
+
+        elif angle != -1 and self.label_response3.cget("text") != "":
+            self.label_response3.configure(text = "")
 
     def spiro_remove(self):
         print(2)
@@ -136,13 +204,32 @@ class GUI:
 
 
 def check_num(entry, label, response, lower_limit, upper_limit, integer):
-    print(12)
+    """Can be applied to any integer the user inputs to check that it's a valid
+    integer that's in range. Uses parameters so each time this function is
+    called there can be customised prompts, ranges, etc.
+    """
+    # Try and except statements test for an invalid input
+    try:
+        choice = integer(entry.get())
+        # Checks that the choice is in range
+        if choice < lower_limit or choice > upper_limit:
+            label.configure(text = response)
+            return -1
+        else:
+            return choice
+    except ValueError:
+        if integer == int:
+            label.configure(text = "Not a valid integer.")
+        elif integer == float:
+            label.configure(text = "Not a valid number.")
+        return -1
 
 # Creates list of spiros for the program to add to
 spiros = []
 # Creates constant for the program to use. Defining here makes it easy to
 # edit. Minimum choice used as constant because no input in the program should
 # Require an input less than 1.
+MAX_SPIRO = 30
 MIN_CHOICE = 1
 
 def main():
